648b5f488d8d42734094f317f2bafd281fe13c4a
Bashar <Bashar@gmail.com>
2025-10-12 00:32:34 +0600

    04: Authentication Ui

--- src/lib/utils.js
+++ src/lib/utils.js
@@ -1,11 +1,11 @@
 import { clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-/**
- * Combine Tailwind class names conditionally.
- */
+
+
 export function cn(...inputs) {
   return twMerge(clsx(inputs));
 }
+
 
 
