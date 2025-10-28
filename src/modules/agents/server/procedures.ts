b742348be1933a7b5e971b6ca3ca3648c85bc805
Bashar <Bashar@gmail.com>
2025-10-29 01:00:35 +0600

    09: Agents Setup

--- src/modules/agents/server/procedures.ts
+++ src/modules/agents/server/procedures.ts
@@ -1,3 +1,4 @@
+// import { TRPCError } from "@trpc/server";
 import { db } from "../../../db";
 import { agents } from "../../../db/schema";
 import { createTRPCRouter, baseProcedure } from "../../../trpc/init";
@@ -7,9 +8,12 @@ export const agentsRouter = createTRPCRouter ({
     getMany: baseProcedure.query( async => {
         const data = await db
             .select()
-            .from(agents)
+            .from(agents);
 
-    return data
+        // await new Promise((resolve) => setTimeout (resolve, 5000));
+        // throw new TRPCError({code: "BAD_REQUEST" });
+
+    return data;
     }),
 });
 
